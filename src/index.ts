bacf03bdd89e3859a39ddc521475fadb57f6bb85
jhsware <sebastian@urbantalk.se>
2021-11-15 17:08:24 +0100

    JIT-transpiling dev server with auto restart

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
-function multiply(first: number, second: number): number {
-  return first * second
-}
-export { multiply }
+import dotenv from 'dotenv'
+import express from 'express'
+import { certificateFor } from 'devcert'
+import https from 'https'
+import http from 'http'
+
+const PORT = 8080
+const SSL = true
+
+dotenv.config()
+const server = express()
+
+server.use((req, res, next) => {
+  console.log(req.pathname)
+  next()
+})
+
+server.get('/', (req, res) => {
+  return res.send('pong')
+})
+
+;(async function main() {
+  if (SSL) {
+    const sslCerts = await certificateFor('localdev.kth.se', {
+      skipHostsFile: true, // Don't modify /etc/hosts
+      skipCertutil: true, // This option will tell devcert to avoid installing certutil tooling.
+    })
+    https.createServer(sslCerts, server).listen(PORT, () => {
+      console.info(`Started HTTPS server on https://localhost:${PORT}`)
+    })
+  } else {
+    http.createServer(server).listen(PORT, () => {
+      console.info(`Started HTTP server on http://localhost:${PORT}`)
+    })
+  }
+})()
